d12f6bc248278d8c41880f0de44d1d0c2835e826
Evarinou <evam.schaller@gmail.com>
2025-01-30 16:49:00 +0100

    Test Build 2

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+);
+
+
+// Umschließe deine App mit BrowserRouter
+ReactDOM.render(
+  <BrowserRouter basename="/">
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
