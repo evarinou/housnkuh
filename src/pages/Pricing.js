461443c8a533113be2e5a699d2af9ef11be11a9f
Evarinou <evam.schaller@gmail.com>
2025-02-25 23:56:03 +0100

    nochamal rental usw

--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Card, CardHeader, CardTitle, CardContent } from '../components/ui/card';
 import { Badge } from '../components/ui/badge';
 import { Store, Box, PackageSearch, ArrowUp, Info, Mail } from 'lucide-react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import RentalRequestForm from '../components/RentalRequestForm';
 
 // ScrollToTop Komponente
